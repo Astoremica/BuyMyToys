99026abd7d808f7d5aab9ae67ffd9a4fc54b84c7
NakamuraY <50987681+Astoremica@users.noreply.github.com>
2019-12-09 21:44:34 +0900

    no message

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php
 require_once './func/function.php';
+require_once './tpl/header/header.php';
--- tpl/header/header.php
+++ tpl/header/header.php
@@ -5,8 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BuyMyToys</title>
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:100,500,900&amp;subset=japanese" rel="stylesheet">
+    <link href="./images/logo.PNG">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+
+    <script src="../../jquery-3.4.1.min.js"></script>
 </head>
 
 <body>
     <header>
+        <h1 class="logo">
+            <a href="">
+                <img src="./images/materials/toys_boy.png" alt="BuyMyToys">
+            </a>
+        </h1>
+        <nav>
+            <ul>
+                <li><a href="">会員登録</a></li>
+            </ul>
+        </nav>
     </header>
