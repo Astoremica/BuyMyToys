8a1b79d66895ffeb90f2cef000336c532463bc63
NakamuraY <50987681+Astoremica@users.noreply.github.com>
2020-01-20 09:33:24 +0900

    会員登録まで完了しました。

--- js/others.js
+++ js/others.js
@@ -6,4 +6,16 @@ $(document).ready(function() {
         $("#login_form").removeClass('active_form');
     });
 
+    // $("#favo_icon").click(function() {
+    //     if (!$(this.hasClass('fav'))) {
+    //         $(this)
+    //             .addClass('fav')
+    //             .attr('src', './images/materials/heart_fav.png')
+    //     } else {
+    //         $(this)
+    //             .removeClass('fav')
+    //             .attr('src', './images/materials/heart.png')
+    //     }
+    // });
+
 });
--- tpl/regist/complete.php
+++ tpl/regist/complete.php
@@ -2,26 +2,37 @@
 <html>
 
 <head>
-    <title>会員登録完了画面</title>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- ホーム画面に追加時アドレスバー非表示 -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <link rel="stylesheet" href="./css/reset.css">
+    <!-- 会員登録時CSS -->
+    <link rel="stylesheet" href="./css/regist_style.css">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:100,500,900&amp;subset=japanese" rel="stylesheet">
+    <link href="./images/materials/toys_boy_white.png">
+    <title>会員登録完了画面</title>
 </head>
 
 <body>
 
-    <?php if (count($errors) === 0) : ?>
-        <h1>会員登録完了画面</h1>
+    <header>
+        <h1>会員登録</h1>
+        <p id="back_button"><a href="#" onclick="history.back(); return false;"><img src="./images/materials/back_arrow.png" alt="戻る"></a></p>
+    </header>
+    <div id="message">
+        <?php if (count($errors) === 0) : ?>
 
-        <p>登録完了いたしました。ログイン画面からどうぞ。</p>
-        <p><a href="">ログイン画面（未リンク）</a></p>
+            <p id="send_mail_message">登録完了いたしました。<br><a href="<?php echo BASE_URL; ?>index.php">トップページへ</a></p>
 
-    <?php elseif (count($errors) > 0) : ?>
+        <?php elseif (count($errors) > 0) : ?>
 
-        <?php foreach ($errors as $value) : ?>
-            <p><?php echo $value; ?></p>
-        <?php endforeach; ?>
+            <?php foreach ($errors as $value) : ?>
+                <p id="send_mail_message"><?php echo $value; ?></p>
+            <?php endforeach; ?>
 
-    <?php endif; ?>
-
+        <?php endif; ?>
+    </div>
 </body>
 
 </html>
